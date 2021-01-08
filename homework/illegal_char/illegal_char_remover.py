8b7b9f66c47e9a0d5a191fdfe0274b2b30f25f50
revire <riva.revekka@gmail.com>
2021-01-08 22:49:38 +0300

    third task done

--- homework/illegal_char/illegal_char_remover.py
+++ homework/illegal_char/illegal_char_remover.py
@@ -1,4 +1,9 @@
 from pyspark.sql.dataframe import DataFrame
+import sys
+from pyspark import Row
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, lit, expr, regexp_replace
+
 from typing import List
 
 
@@ -7,7 +12,29 @@ class IllegalCharRemover:
     Class provides possibilities to remove illegal chars from string column.
     """
     def __init__(self, chars: List[str], replacement):
-        pass
+        print(chars, replacement)
+
+
+        if chars:
+            self.chars = chars
+        else:
+            raise ValueError
+
+        if replacement is not None:
+            self.replacement = replacement
+        else:
+            raise ValueError
 
     def remove_illegal_chars(self, dataframe: DataFrame, source_column: str, target_column: str):
-        pass
+        escapes = ['\\','.','+','*','?','[','^',']','$','(',')','{','}','=','!','<','>','|',':']
+        chars = ['\\'+char if char in escapes else char for char in self.chars]
+        print(chars)
+        chars = ''.join(chars)
+        dataframe = dataframe\
+            .withColumn(source_column, regexp_replace(source_column, fr'[{chars}]', self.replacement))\
+            .withColumnRenamed(source_column, target_column)
+
+        for d in dataframe.collect():
+            print(d)
+
+        return dataframe
