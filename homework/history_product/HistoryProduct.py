9262b4d96929801431ee6a8c53d2d0f665255709
revire <riva.revekka@gmail.com>
2021-01-08 16:10:03 +0300

    second task done

--- homework/history_product/HistoryProduct.py
+++ homework/history_product/HistoryProduct.py
@@ -1,5 +1,5 @@
 from pyspark.sql import DataFrame
-from pyspark.sql.functions import col, lit, when
+from pyspark.sql.functions import col, lit, expr
 
 
 
@@ -20,59 +20,37 @@ class HistoryProduct:
 
     # ToDo: Implement history product
     def get_history_product(self, old_dataframe: DataFrame, new_dataframe: DataFrame):
-        pk = self.primary_keys[0]
+
         cols = old_dataframe.columns
 
+        try:
+            pk = self.primary_keys
+            expressions = []
+            for p in pk:
+                expressions.append(f'old.{p} <=> new.{p}')
+            expression = ' and '.join(expressions)
+        except TypeError:
+            expressions = []
+            for c in cols:
+                expressions.append(f'old.{c} <=> new.{c}')
+            expression = ' and '.join(expressions)
 
-        # result_dataframe = old_dataframe\
-        #     .unionAll(new_dataframe.exceptAll(old_dataframe))\
-        #     .dropDuplicates([pk])\
-        #     .alias('resdf')\
-        #     .sort([pk])
-
-        # old_cols = old_dataframe.columns
-        # new_old_cols = ['old_'+x if x != pk else x for x in old_cols]
-        # old_names_tuples = zip(old_cols, new_old_cols)
-        # for old_name in old_names_tuples:
-        #     old_dataframe = old_dataframe.withColumnRenamed(old_name[0], old_name[1])
-        #
-        # new_cols = new_dataframe.columns
-        # new_new_cols = ['new_' + x if x != pk else x for x in new_cols]
-        # new_names_tuples = zip(new_cols, new_new_cols)
-        # for new_name in new_names_tuples:
-        #     new_dataframe = new_dataframe.withColumnRenamed(new_name[0], new_name[1])
-        #
-        # new_names = list(zip(new_old_cols, new_new_cols))
-        # print(new_names)
-        #
-        # result_dateframe = old_dataframe\
-        #     .unionAll(new_dataframe.exceptAll(old_dataframe))
-        #
-        # full_dataframe = old_dataframe.join(other=new_dataframe, on='id', how='full').alias('fulldf')
-        #
-        # for names in new_names:
-        #     print(names)
-        #     full_dataframe = full_dataframe.withColumn('meta',
-        #                               when(col(names[0])==col(names[1]), 'not_changed')
-        #                               .when(col(names[0]).isNull(), 'inserted')
-        #                               .when(col(names[1]).isNull(), 'deleted')
-        #                               .otherwise('changed'))
-        #
-        # result_dataframe = result_dataframe.join(full_dataframe, on='id', how='left').select('resdf.*', 'meta')
-        #
-        # resdf = result_dataframe.collect()
-        # # fulldf = full_dataframe.collect()
-        # for d in resdf:
-        #     print(d)
+        not_changed_keys = []
+        for c in cols:
+            not_changed_keys.append(f'old.{c} <=> new.{c}')
+        not_changed_expression = ' and '.join(not_changed_keys)
 
 
 
-        not_changed_rows = old_dataframe.join(new_dataframe, on=cols, how='semi')
+        old_dataframe = old_dataframe.alias('old')
+        new_dataframe = new_dataframe.alias('new')
 
-        changed_rows_new = new_dataframe.join(old_dataframe, on=pk, how='semi')\
+        not_changed_rows = old_dataframe.join(new_dataframe, expr(not_changed_expression), how='semi')
+
+        changed_rows_new = new_dataframe.join(old_dataframe, expr(expression), how='semi')\
             .exceptAll(not_changed_rows)
 
-        changed_rows_old = old_dataframe.join(new_dataframe, on=pk, how='semi') \
+        changed_rows_old = old_dataframe.join(new_dataframe, expr(expression), how='semi') \
             .exceptAll(not_changed_rows)
 
         old_rows = old_dataframe\
@@ -99,17 +77,17 @@ class HistoryProduct:
         #
         # for r in new_rows.collect():
         #     print(r)
+        # print('=============================================')
+
 
         result_dateframe = not_changed_rows\
                             .union(changed_rows)\
                             .union(old_rows)\
                             .union(new_rows)\
-                            .sort('id')
+                            .sort('id', 'name')
 
-        #
-        resdf = result_dateframe.collect()
-        for d in resdf:
-            print(d)
-
+        # resdf = result_dateframe.collect()
+        # for d in resdf:
+        #     print(d)
 
         return result_dateframe
