dc7b5a2e58be325e025d947f49e7c032d665b1dd
Margaev <andreymargaev@gmail.com>
2021-01-14 13:13:52 +0300

    StringTypeTransformer implemented

--- homework/type_transformations/string_type_transformer.py
+++ homework/type_transformations/string_type_transformer.py
@@ -1,5 +1,7 @@
 from pyspark.sql import DataFrame
-from pyspark.sql.types import StructType
+from pyspark.sql.functions import col, lit, to_date, to_timestamp, when
+from pyspark.sql.types import StructType, DateType, TimestampType, BooleanType
+
 
 class StringTypeTransformer:
     """
@@ -7,4 +9,24 @@ class StringTypeTransformer:
     """
 
     def transform_dataframe(self, dataframe: DataFrame, expected_schema: StructType):
-        pass
+        # transforming dataframe according to expected schema
+        for f in expected_schema.fields:
+            if isinstance(f.dataType, DateType):
+                dataframe = dataframe.withColumn(f.name, to_date(col(f.name), 'd-M-y'))
+            elif isinstance(f.dataType, TimestampType):
+                dataframe = dataframe.withColumn(f.name, to_timestamp(col(f.name), 'd-M-y H:m:s'))
+            elif isinstance(f.dataType, BooleanType):
+                dataframe = dataframe.withColumn(f.name, when(
+                    col(f.name).isin(['true', 'True', '1', 'y', 'Y', 'yes', 'Yes']),
+                    lit('true').cast(BooleanType())
+                ).otherwise(lit('false').cast(BooleanType())))
+                dataframe.schema[f.name].nullable = False
+            else:
+                dataframe = dataframe.withColumn(f.name, col(f.name).cast(f.dataType))
+
+        # updating schema
+        for c in dataframe.schema:
+            if isinstance(c.dataType, BooleanType):
+                c.nullable = False
+
+        return dataframe
